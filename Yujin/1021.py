7821ed86da37da77c31be62ef0d0de7f5b2e4521
rohujin97 <rohujin98@naver.com>
2021-08-04 21:30:19 +0900

    1021 add comments

--- Yujin/1021.py
+++ Yujin/1021.py
@@ -8,16 +8,16 @@ cnt = 0
 for i in range(M):
     length = len(q)
     index = q.index(target[i])
-    if index < length - index:
+    if index < length - index: #찾는 인덱스가 중간보다 가깝다면
         while True:
-            if q[0] == target[i]:
+            if q[0] == target[i]: # 처음 위치에 왔을때 pop
                 q.pop(0)
                 break
-            else:
+            else: # 처음 위치로 갈때까지 계속 이동
                 q.append(q[0])
                 q.pop(0)
                 cnt += 1
-    else:
+    else: # 찾는 인덱스가 중간과 같거나 멀다면
         while True:
             if q[0] == target[i]:
                 q.pop(0)
