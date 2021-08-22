0f5750d2368f6f056829824d4295c8f576cd2707
rohujin97 <rohujin98@naver.com>
2021-08-22 21:03:46 +0900

    Yujin

--- Yujin/프로그래머스/42860_조이스틱.py
+++ Yujin/프로그래머스/42860_조이스틱.py
@@ -2,21 +2,22 @@
 def solution(name):
     alpha = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     res = [0 for _ in range(len(name))]
-    cnt = 0
     for i in range(len(name)):
         res[i]= alpha[(ord(name[i]) - ord("A"))]
-    answer = sum(res)
 
-    size = len(name)
-    lr = size - 1
-    for i in range(size):
-        index = i + 1
-        while index < size and res[index] == 0:
-            index += 1
-        tmp = min(i, size - index)
-        lr = min(lr, i + size - index + tmp)
-
-    answer += lr
-    print(answer)
-
-solution("ABAAAAAAAAABB")
+    answer = 0
+    index = 0
+    while True:
+        answer += res[index]
+        res[index] = 0
+        if sum(res) == 0:
+            print(answer)
+            return answer
+        left, right = 1, 1
+        while res[index - left] == 0:
+            left += 1
+        while res[index + right] == 0:
+            right += 1
+        
+        answer += left if left < right else right
+        index += -left if left < right else right
