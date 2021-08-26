1827629f9262a69b050d5203bf7e829eb0a6c783
rohujin97 <rohujin98@naver.com>
2021-08-27 00:04:32 +0900

    14500 solved

---
+++ Yujin/백준/14500_테트로미노.py
@@ -0,0 +1,41 @@
+d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+dd = [[(0, -1), (-1, 0), (0, 1)], [(0, -1), (1, 0), (0, 1)], [(-1, 0), (0, 1), (1, 0)], [(-1, 0), (0, -1), (1, 0)]]
+
+def notdfs(r, c):
+    global result
+    for dir in dd:
+        total = matrix[r][c]
+        for i in range(3):
+            nr = r + dir[i][0]
+            nc = c + dir[i][1]
+            if 0 <= nr < N and 0 <= nc < M:
+                total += matrix[nr][nc]
+            else:
+                break
+        result = max(result, total)
+
+def dfs(r, c, total, visited):
+    global result
+    if len(visited) == 4:
+        result = max(result, total)
+        return
+    for i in range(4):
+        nr = r + d[i][0]
+        nc = c + d[i][1]
+        if 0 <= nr < N and 0 <= nc < M:
+            if (nr, nc) not in visited:
+                visited.append((nr, nc))
+                dfs(nr, nc, total + matrix[nr][nc], visited)
+                visited.pop()
+
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+result = 0
+
+for r in range(N):
+    for c in range(M):
+        dfs(r, c, matrix[r][c], [(r, c)])
+        notdfs(r, c)
+print(result)
+
+
