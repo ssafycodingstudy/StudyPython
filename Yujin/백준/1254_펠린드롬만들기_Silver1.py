3523250a516448ca964b2bd8dfe977cfc8c1029e
rohujin97 <rohujin98@naver.com>
2021-09-21 12:17:29 +0900

    1254 solved

---
+++ Yujin/백준/1254_펠린드롬만들기_Silver1.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+S = list(input().strip())
+dp = []
+for i in range(len(S)):
+    R = []
+    for j in range(len(S)-1, i-1, -1): # 앞에서 부터 하나씩 줄여나감
+        R.append(S[j])
+    if S[i:len(S)] == R[:len(S)]: # 펠린드롬인것을 찾으면
+        dp = S[:i] # 펠린드롬 아닌 부분을 저장
+        break
+dp.reverse()
+S = S + dp # 주어진 문자열에 펠린드롬 아닌 부분을 더함
+print(len(S))
