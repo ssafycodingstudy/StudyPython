3cd26f493ddcd50b950a803b1f8a099f795f8a38
rohujin97 <rohujin98@naver.com>
2021-09-28 21:19:01 +0900

    43105

--- Yujin/프로그래머스/43105_정수삼각형_Lv3.py
+++ Yujin/프로그래머스/43105_정수삼각형_Lv3.py
@@ -2,16 +2,15 @@ triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 
 def solution(triangle):
     dp = triangle[:]
-    lenT = len(triangle)
-    for i in range(1, lenT):
+    for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
             if j == 0:
                 dp[i][j] = dp[i-1][j] + dp[i][j]
             elif j == len(triangle[i])-1:
                 dp[i][j] = dp[i-1][j-1] + dp[i][j]
             else:
-                dp[i][j] = dp[i-1][j] + dp[i][j] if dp[i-1][j] + dp[i][j] > dp[i-1][j-1] + dp[i][j] else dp[i-1][j-1] + dp[i][j]
+                dp[i][j] = max(dp[i-1][j] + dp[i][j], dp[i-1][j-1] + dp[i][j])
 
-    return max(dp[lenT-1])
+    return max(dp[-1])
 
 print(solution(triangle))
