f3c38759d51cfc9c5f0e54d5d4f9648e3b993fe7
hk <khman119@gmail.com>
2021-10-26 01:29:54 +0900

    BOJ_2252_줄_세우기_solved

---
+++ Hyokyun/백준/2252_줄_세우기.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+indegree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+
+def topology_sort():
+    q = deque([])
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+    while q:
+        now = q.popleft()
+        print(now, end=' ')
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+
+topology_sort()
