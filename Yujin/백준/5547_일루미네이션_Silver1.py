f9002d9182ebc85cae60eba18b83b02c49aded4b
rohujin97 <rohujin98@naver.com>
2021-12-01 22:32:47 +0900

    5547 일루미네이션 solved

--- Yujin/백준/5547_일루미네이션_Silver1.py
+++ Yujin/백준/5547_일루미네이션_Silver1.py
@@ -2,44 +2,52 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-dr1, dr2 = [1, 1, 0, 0, -1, -1], [0, 1, -1, 1, 0, 1]
+dr1, dr2 = [1, 1, 0, 0, -1, -1], [1, 1, 0, 0, -1, -1]
 dc1, dc2 = [0, 1, -1, 1, 0, 1], [-1, 0, -1, 1, -1, 0]
 
-def bfs(r, c):
-    q = deque([(r, c)])
-    visited[r][c] = True
-    while q:
-        r, c = q.popleft()
-        if r == 0 or r % 2 == 0:
-            for k in range(6):
-                nr = r + dr1[k]
-                nc = c + dc1[k]
-                if 0 > nr or nr >= H or 0 > nc or nc >= W: continue
-                if house[nr][nc] == 1:
-                    print(r, c, nr, nc)
-                    wall[r][c] += 1
-                    if not visited[nr][nc]:
-                        q.append((nr, nc))
-                        visited[nr][nc] = True
-        else:
-            for k in range(6):
-                nr = r + dr2[k]
-                nc = c + dc2[k]
-                if 0 > nr or nr >= H or 0 > nc or nc >= W: continue
-                if house[nr][nc] == 1:
-                    print(r, c, nr, nc)
-                    wall[r][c] += 1
-                    if not visited[nr][nc]:
-                        q.append((nr, nc))
-                        visited[nr][nc] = True
+def check(dr, dc, r, c, i):
+    nr = r + dr[i]
+    nc = c + dc[i]
+    if 0 <= nr <= H+1 and 0 <= nc <= W+1:
+        if not visited[nr][nc]:
+            if house[nr][nc] == 0:
+                visited[nr][nc] = True
+                q.append((nr, nc))
 
 W, H = map(int, input().split())
-house = [list(map(int, input().split())) for _ in range(H)]
-wall = [[0]*W for _ in range(H)]
-visited = [[False]*W for _ in range(H)]
+house = [[0]*(W+2) for _ in range(H+2)]
+visited = [[False]*(W+2) for _ in range(H+2)]
 
-for i in range(H):
-    for j in range(W):
-        if house[i][j] == 1 and not visited[i][j]:
-            bfs(i, j)
-print(*wall, sep="\n")
+for i in range(1, H+1):
+    tmp = list(map(int, input().split()))
+    for j in range(1, W+1):
+        house[i][j] = tmp[j-1]
+
+q = deque([(0, 0)])
+visited[0][0] = True
+
+while q:
+    r, c = q.popleft()
+    for i in range(6):
+        if r % 2 == 0:
+            check(dr2, dc2, r, c, i)
+        else:
+            check(dr1, dc1, r, c, i)
+
+answer = 0
+for i in range(1, H+1):
+    for j in range(1, W+1):
+        if house[i][j] == 0: continue
+        for k in range(6):
+            if i % 2 == 0:
+                nr = i + dr2[k]
+                nc = j + dc2[k]
+            else:
+                nr = i + dr1[k]
+                nc = j + dc1[k]
+            if visited[nr][nc]: answer += 1
+print(answer)
+
+
+
+
