8c4a60dffd38391f22d6e9d02c0740def4c6671a
rohujin97 <rohujin98@naver.com>
2021-12-08 22:14:15 +0900

    modify 17626

--- Yujin/백준/17626_FourSquares_Silver4.py
+++ Yujin/백준/17626_FourSquares_Silver4.py
@@ -5,16 +5,10 @@ def fourSquares(N):
     if (N**0.5).is_integer():
         return 1
     for i in range(1, int(N**0.5)+1):
-        if N < i ** 2:
-            break
         if ((N - i ** 2) ** 0.5).is_integer():
             return 2
     for i in range(1, int(N**0.5) + 1):
-        if N < i ** 2:
-            break
         for j in range(1, int((N - i ** 2) ** 0.5) + 1):
-            if N < i ** 2 + j ** 2:
-                break
             if ((N - i ** 2 - j ** 2) ** 0.5).is_integer():
                 return 3
     return 4
